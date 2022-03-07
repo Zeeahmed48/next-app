5e4132abcc9ad8930d3ab4a1677565eb7c41da0c
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.com>
2022-03-07 09:38:23 -0600

    form styles

--- components/meetups/NewMeetUpForm.js
+++ components/meetups/NewMeetUpForm.js
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
+import classes from "./NewMeetupForm.module.css";
 
-const NewMeetUpForm = ({ onAddMeetup }) => {
+const NewMeetupForm = ({ onAddMeetup }) => {
   const [meetupData, setMeetupData] = useState({
-    title: '',
-    description: '',
-    image: '',
-    address: '',
+    title: "",
+    description: "",
+    image: "",
+    address: "",
   });
 
   const handleMeetupChange = (type, event) => {
@@ -18,50 +19,52 @@ const NewMeetUpForm = ({ onAddMeetup }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (typeof onAddMeetup === 'function') onAddMeetup(meetupData);
+    if (typeof onAddMeetup === "function") onAddMeetup(meetupData);
   };
 
   const { title, description, image, address } = meetupData;
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form onSubmit={handleSubmit} className={classes.formContainer}>
+      <div className={classes.inputWrapper}>
+        <label>Meetup Title</label>
         <input
-          onChange={(e) => handleMeetupChange('title', e)}
+          onChange={(e) => handleMeetupChange("title", e)}
           value={title}
-          type='text'
-          placeholder='Meetup Title'
+          type="text"
         />
       </div>
-      <div>
+
+      <div className={classes.inputWrapper}>
+        <label>Meetup Image</label>
         <input
-          onChange={(e) => handleMeetupChange('description', e)}
-          value={description}
-          type='text'
-          placeholder='Meetup Description'
-        />
-      </div>
-      <div>
-        <input
-          onChange={(e) => handleMeetupChange('image', e)}
+          onChange={(e) => handleMeetupChange("image", e)}
           value={image}
-          type='text'
-          placeholder='Meetup Image'
+          type="text"
         />
       </div>
-      <div>
+
+      <div className={classes.inputWrapper}>
+        <label>Address</label>
         <input
-          onChange={(e) => handleMeetupChange('address', e)}
+          onChange={(e) => handleMeetupChange("address", e)}
           value={address}
-          type='text'
-          placeholder='Meetup Address'
+          type="text"
         />
       </div>
-      <div>
-        <button type='submit'>Add Meetup</button>
+      <div className={classes.inputWrapper}>
+        <label>Description</label>
+        <input
+          onChange={(e) => handleMeetupChange("description", e)}
+          value={description}
+          type="text"
+        />
+      </div>
+      <div className={classes.buttonWrapper}>
+        <button type="submit">Add Meetup</button>
       </div>
     </form>
   );
 };
 
-export default NewMeetUpForm;
+export default NewMeetupForm;
