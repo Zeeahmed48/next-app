e82c7b76f1bb410c315828cecf13965d40f490a2
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.com>
2022-03-11 02:27:30 +0500

    all meetings API

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,51 @@
-import MeetupsList from "../components/meetups/MeetupList.js";
+import { MongoClient } from 'mongodb';
+import MeetupList from '../components/meetups/MeetupList.js';
 
 const DUMMY_MEETUPS = [
   {
-    id: "meetup1",
-    title: "First Meetup.",
+    id: 'meetup1',
+    title: 'First Meetup.',
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU",
-    address: "Las Vegas",
-    description: "A meetup hold in las vegas.",
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU',
+    address: 'Las Vegas',
+    description: 'A meetup hold in las vegas.',
   },
   {
-    id: "meetup2",
-    title: "Second Meetup.",
+    id: 'meetup2',
+    title: 'Second Meetup.',
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU",
-    address: "Las Vegas",
-    description: "A meetup hold in las vegas.",
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU',
+    address: 'Las Vegas',
+    description: 'A meetup hold in las vegas.',
   },
 ];
 
 const HomePage = (props) => {
-  return <MeetupsList meetups={props.meetups} />;
+  return <MeetupList meetups={props.meetups} />;
 };
 
 export const getStaticProps = async () => {
+  const uri = process.env.REACT_APP_MONGO_URI;
+
+  const client = new MongoClient(uri);
+
+  await client.connect();
+
+  const db = client.db('meetups');
+  const meetupsCollections = db.collection('meetups');
+  const meetups = await meetupsCollections.find({}).toArray();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        description: meetup.description,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
     },
+    revalidate: 1,
   };
 };
 
