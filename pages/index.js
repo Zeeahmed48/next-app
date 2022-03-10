9fb1aace17b14afafb1e416c56f425a09d41cabf
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.com>
2022-03-11 02:28:29 +0500

    removed dummmy meetings

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,6 @@
 import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList.js';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'meetup1',
-    title: 'First Meetup.',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU',
-    address: 'Las Vegas',
-    description: 'A meetup hold in las vegas.',
-  },
-  {
-    id: 'meetup2',
-    title: 'Second Meetup.',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTXvo15izkAl8A9f1KfV9LRbxX102OMLM-2A&usqp=CAU',
-    address: 'Las Vegas',
-    description: 'A meetup hold in las vegas.',
-  },
-];
-
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
