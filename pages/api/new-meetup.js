a1f61f46bcd245d6d032e176d184f53f7fe14399
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.com>
2022-03-11 00:17:53 +0500

    add meetup API

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,22 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body;
-        res.status(200).json(data);
-    }
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.REACT_APP_MONGO_URI;
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    const data = req.body;
+    const client = new MongoClient(uri);
+
+    await client.connect();
+
+    const db = client.db('meetups');
+    const meetupsCollections = db.collection('meetups');
+    await meetupsCollections.insertOne(data);
+
+    await client.close();
+
+    res.status(201).json({ message: 'Meetup Sucessfully Created!' });
+  }
 };
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,14 +1,20 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
+  const router = useRouter();
+
   const addNewMeetup = async (meetupData) => {
-    const response = await fetch('/api/new-meetup', {
+    fetch('/api/new-meetup', {
       method: 'POST',
-      body: JSON.stringify(meetupData)
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
-    const data = await  response.json();
-    console.log("data", data);
+    router.push('/');
   };
+
   return <NewMeetupForm onAddMeetup={addNewMeetup} />;
 };
 
