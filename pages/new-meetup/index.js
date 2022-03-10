e63fe84f9d06a47668e83d2d92c28d7075ca6b37
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.com>
2022-03-10 10:21:10 -0600

    added basic API

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,13 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
-  const addNewMeetup = (meetupData) => {
-    console.log('meetupData', meetupData);
+  const addNewMeetup = async (meetupData) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(meetupData)
+    });
+    const data = await  response.json();
+    console.log("data", data);
   };
   return <NewMeetupForm onAddMeetup={addNewMeetup} />;
 };
